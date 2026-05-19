32c2de3124e2e200243a7eb9bbbd24685437d277
thangvu <vietthangvunguyen@MacBook-Pro.local>
2026-05-19 15:40:17 +0700

    feat: wrap SduiScreen in ValueListenableBuilder for observable state

--- packages/desk_sdui_demo/lib/main.dart
+++ packages/desk_sdui_demo/lib/main.dart
@@ -3,9 +3,16 @@ import 'dart:io' show ProcessInfo;
 import 'package:desk_sdui/desk_sdui.dart';
 import 'package:desk_sdui_demo/desk_sdui_setup.g.dart';
 import 'package:desk_sdui_demo/screens/counter_demo.dart';
+import 'package:fdb_helper/fdb_helper.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const DemoApp());
+void main() {
+  if (!kReleaseMode) {
+    FdbBinding.ensureInitialized();
+  }
+  runApp(const DemoApp());
+}
 
 class DemoApp extends StatefulWidget {
   const DemoApp({super.key});
@@ -15,7 +22,7 @@ class DemoApp extends StatefulWidget {
 
 class _DemoAppState extends State<DemoApp> {
   late final Runtime rt;
-  late final Counter _counter;
+  late final ValueNotifier<Counter> _counter;
   late final CounterActions _actions;
   bool _darkMode = true;
 
@@ -34,8 +41,8 @@ class _DemoAppState extends State<DemoApp> {
         'RSS delta ${(rssAfter - rssBefore) ~/ 1024} KB '
         '(before ${rssBefore ~/ 1024} KB, after ${rssAfter ~/ 1024} KB)');
 
-    _counter = Counter();
-    _actions = CounterActions();
+    _counter = ValueNotifier(Counter());
+    _actions = CounterActions(_counter);
   }
 
   void _toggleTheme() => setState(() => _darkMode = !_darkMode);
@@ -58,10 +65,15 @@ class _DemoAppState extends State<DemoApp> {
             ),
           ],
         ),
-        body: SduiScreen(
-          runtime: rt,
-          name: 'counter_demo',
-          inputs: {'c': _counter, 'a': _actions},
+        body: ValueListenableBuilder<Counter>(
+          valueListenable: _counter,
+          builder: (context, counter, _) {
+            return SduiScreen(
+              runtime: rt,
+              name: 'counter_demo',
+              inputs: {'c': counter, 'a': _actions},
+            );
+          },
         ),
       ),
     );
